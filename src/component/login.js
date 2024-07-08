5ec0f24f4469cf81ad820c052abd72d031ead693
mamad-coder <mamadabd04@gmail.com>
2024-07-08 12:59:27 +0330

    .

---
+++ src/component/login.js
@@ -0,0 +1,16 @@
+const Login =()=>{
+    return(
+        <div>
+           
+    <button> 
+        تایید
+    </button>
+    <input placeholder="شماره همراه"> </input>
+    <input placeholder="کد تصویر"></input>
+    
+        
+        </div>
+        
+    )
+} 
+export default Login
